01f19f8466ab0109f5606b6273f70d82f98cc50c
paolow24 <paolonava24@gmail.com>
2025-02-10 10:50:46 +0800

    Create practicalexam1.py

---
+++ python/practicalexam1.py
@@ -0,0 +1,39 @@
+# print("hello")
+# print('hello')
+
+# a = "hello"
+# print(a)
+
+
+# a = "hello, world!"
+# print(a[-6:-2])
+
+# a = "Hello, World!"
+# print(len(a))
+
+# a = " Hello, World!"
+# print(a.strip())
+
+# a = "Hello, World!"
+# print(a.replace("H", "B"))
+
+# a = "Hello, World!"
+# b = a.split("e")
+# print(b)
+
+# txt = "The rain in Spain stays mainly in the plain"
+# x = "ain" not in txt
+# print(x)
+
+# a = 20
+# b = "My name is Paolo, and I am {}"
+# print(b.format(a))
+
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = "I want {} pieces of item number {} for {} dollars."
+# print(myorder.format(quantity, price, itemno))
+
+
+
